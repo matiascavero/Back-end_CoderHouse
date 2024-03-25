82d905c166802e37f37b2d43bc69705174d5568b
matiascavero <admsmartmatias@gmail.com>
2024-03-25 20:50:25 -0300

    Desafio 3 (arreglamos el duplicado de instancias y agregamos una verificacion del id)

--- src/app.js
+++ src/app.js
@@ -4,23 +4,29 @@ import ProductManager from './productClass.js';
 const app = express();
 const PORT = 3000;
 
-app.get('/productos', async(req, res)=>{
-    const {limit} = req.query;
-    const prod = new ProductManager()
+app.get('/', (req, res) => {
+    res.send('hola esta es la pagina principal')
+})
+
+const prod = new ProductManager();
+
+app.get('/productos', async (req, res) => {
+    const { limit } = req.query;
     const producto = await prod.getProducts(limit)
     res.json(producto)
 
 })
 
-
-app.get('/productos/:pid', async (req, res)=>{
-    const {pid} = req.params;
-    const prod = new ProductManager();
+app.get('/productos/:pid', async (req, res) => {
+    const { pid } = req.params;
+    if(isNaN(pid)){
+        return res.send('Error, el id tiene que ser numerico')
+    }
     const producto = await prod.getProductsId(Number(pid))
     res.json(producto)
 })
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`)
 })
